8765d9494facb03bb07ca4e6fe0fabec2cb67777
JerryHu <jerryhu@JerryHus-Mac-mini.local>
2017-04-06 23:35:57 -0400

    restructure of java fundamentals concurrency multithreading

---
+++ CP_MultiThreading/src/main/java_fundamentals_concurrency_multithreading/com/ps/jfcm/_0102creatingthreads/helloWorld/RealHelloWorldSingleThreaded.java
@@ -0,0 +1,20 @@
+package com.ps.jfcm._0102creatingthreads.helloWorld;
+
+
+public class RealHelloWorldSingleThreaded
+{
+	public static void main(String args[])
+	{
+		String countries[] = { "France", "India", "China", "USA", "Germany" };
+		
+		for (String country : countries)
+		{
+			System.out.println("Hello " + country + "!");
+		}
+		
+		// In Java 8, we could do the following:
+		
+		//Arrays.stream(countries).
+	    //   forEach(country -> System.out.println("Hello " + country + "!"));
+	}
+}
